244e65bbd6e4303f78c138fe6d080b78c3e71867
Stephan Hradek <Stephan.Hradek@ebuconnect.de>
2020-02-20 19:49:49 +0100

    add s.to Auto-Follow to Hoster

---
+++ s.to Auto-Follow to Hoster.user.js
@@ -0,0 +1,82 @@
+// ==UserScript==
+// @name     s.to Auto-Follow to Hoster
+// @version  1
+// @grant    none
+// @include  https://s.to/serie/stream/*/staffel-*/episode-*
+// ==/UserScript==
+
+// These episodes I want to get in english
+var english= [
+	'The Orville',
+  'Death in Paradise',
+  /*'Young Sheldon',*/
+  'The Big Bang Theory',
+  'Doctor Who',
+];
+
+var sep = '__';
+
+function do_stuff() {
+  try {
+    // Get the name of the series
+    var series= document.querySelector('div.hosterSeriesTitle').textContent;
+    
+    // get the season and episode
+    var siteTitle=document.querySelector('div.hosterSiteTitle');
+		var season= siteTitle.getAttribute('data-season');
+		var episode= siteTitle.getAttribute('data-episode');
+    var title= siteTitle.querySelector('h2 :first-child').textContent;
+    
+    // get the current language
+    var lng= document.querySelector("img.selectedLanguage");
+    var currentLanguage = lng.getAttribute("data-lang-key");
+    var seasondir='Staffel';
+
+    // Is it a series watched in english?
+    if ( english.indexOf(series) >= 0 ) {
+      // Do we have it in english?
+      var en= document.querySelector("img[title='Englisch']");
+      if ( en !== null ) {
+        // Yes, so we will take that
+        currentLanguage= en.getAttribute("data-lang-key");
+        title= siteTitle.querySelector('h2 .episodeEnglishTitel').textContent;
+        seasondir='Season';
+      }
+    }
+    
+    // set the window name to the info just found
+    window.name= [series, season, episode, title, seasondir].join(sep).replace(/\s*[:\/]\s*/g, ' - ');
+    
+    // preferred hosters
+    var hosters= [ "Streamcloud", "Openload", "Streamango" ];
+    // was a hoster appended?
+    var hoster= document.location.hash.replace(/^#/, '');
+    // Then push it to the front
+    if (hoster !== "") {
+      hosters.push(hoster);
+    }
+    
+    // Try to find a link for the preferred hoster and language
+    var n= null;
+    while (n === null) {
+      if (hosters.length < 1) {
+        throw "No preferred hoster found";
+      }
+      hoster= hosters.pop();
+      n= document.querySelector("li[data-lang-key='"+currentLanguage+"'] i.icon." + hoster);
+    }
+    console.log(hoster);
+    
+    // go to that page
+    while (n.nodeName !== "LI") {
+      n= n.parentNode;
+    }
+    document.location= n.getAttribute('data-link-target');
+  }
+  catch (e) {
+    console.log(e);
+  }
+}
+
+do_stuff();
+
