3fb9dae3d264c77207a5fb9a55bd4a5700962cca
Stephan Hradek <Stephan.Hradek@ebuconnect.de>
2020-02-22 08:49:58 +0100

    add Bettelseite aus - FAZ

---
+++ Bettelseite aus - FAZ.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name     Bettelseite aus - FAZ
+// @version  1
+// @grant    none
+// @include  https://www.faz.net/*
+// @run-at   document-start
+// ==/UserScript==
+
+// On start of the document, before it is displayed
+// (document-start) run below script
+
+// As soon as the ready state changes to interactive
+document.onreadystatechange = function () {
+  if (document.readyState === "interactive") {
+    // tell the AdBlocker script that everything is fine
+	  unsafeWindow.sptoggleOff= true;
+  }
+}
