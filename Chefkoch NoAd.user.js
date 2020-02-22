bcc3ee0b4e41bfa91c426d53639d059a2d4dd2f5
Stephan Hradek <Stephan.Hradek@ebuconnect.de>
2020-02-22 23:46:59 +0100

    Add Chefkoch Script

--- Chefkoch NoAd.user.js
+++ Chefkoch NoAd.user.js
@@ -4,7 +4,10 @@
 // @grant    none
 // @include  https://www.chefkoch.de/*
 // ==/UserScript==
-$('div').each(function(i,e) {
+
+document.querySelectorAll('div').forEach(function(e) {
+  // check each div
   if (e.textContent.trim() !== 'Ad' ) return;
+  // If the only text is "Ad" - remove the div
   e.remove();
 })
