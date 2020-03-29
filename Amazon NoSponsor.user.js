c5293938026c72b6b30da6956701d353ace9bdc8
Stephan Hradek <Stephan.Hradek@ebuconnect.de>
2020-03-29 22:12:16 +0200

    Amazon Nosponsor im Intervall wiederholen

--- Amazon NoSponsor.user.js
+++ Amazon NoSponsor.user.js
@@ -5,12 +5,19 @@
 // @include  https://www.amazon.de/s?*
 // ==/UserScript==
 
-document
-  .querySelectorAll('div.s-result-item')
-  .forEach(
-  	function(elt) {
-      if (null !== elt.textContent.match(/Gesponsert/)) {
-        elt.parentNode.removeChild(elt);
+window.setTimeout(removeit, 100);
+
+function removeit() {
+  var removed= 0;
+  document
+    .querySelectorAll('div.s-result-item')
+    .forEach(
+      function(elt) {
+        if (null !== elt.textContent.match(/Gesponsert/)) {
+          elt.parentNode.removeChild(elt);
+          ++removed;
+        }
       }
-    }
-  )
+    );
+  if (removed == 0)  window.setTimeout(removeit, 100);
+}
