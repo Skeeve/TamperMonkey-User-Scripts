6c0b0fc9682c38ca8587deff9b5b2e0d5d886b45
Stephan Hradek <stephan.hradek@eco.de>
2021-09-24 13:54:10 +0200

    fixed deleteion of delayed loaded images

--- Chefkoch NoAd.user.js
+++ Chefkoch NoAd.user.js
@@ -1,20 +1,42 @@
 // ==UserScript==
 // @name     Chefkoch NoAd
-// @version  2
+// @version  3
 // @grant    none
 // @include  https://www.chefkoch.de/*
+// @exclude  https://www.chefkoch.de/magazin/*
 // ==/UserScript==
 
-document.querySelectorAll('div').forEach(function(e) {
-  // check each div
-  if (e.textContent.trim() !== 'Ad' ) return;
-  // If the only text is "Ad" - remove the div
-  e.remove();
-});
-document.querySelectorAll('img[referrerpolicy="unsafe-url"]').forEach(function(e) {
-  // check each mage
-  var grandparent = e.parentNode.parentNode;
-  if (grandparent.tagName == 'DIV ) {
-      e.remove();
-  }
-});
+
+(function() {
+    'use strict';
+    var removable = 0;
+    var callsremaining = 100;
+    function waitforAds() {
+        --callsremaining;
+        var removables = document.querySelectorAll('img[referrerpolicy="unsafe-url"]');
+        console.log(callsremaining, removable, removables);
+        if (removables.length != removable || removable < 4) {
+            removable = removables.length;
+            if (callsremaining < 1) return;
+            window.setTimeout(waitforAds, 100);
+            console.log("waiting", document.location);
+            return;
+        }
+        removables.forEach( e => {
+            var grandparent = e.parentNode.parentNode;
+            if (grandparent.tagName == 'DIV' ) {
+                e.remove();
+                console.log(e);
+            }
+        });
+        document.querySelectorAll('div.swiper-slide').forEach( e => {
+            if (e.textContent.match(/^Ad\s/)) e.remove()
+        });
+        document.querySelectorAll('guj-ad').forEach( e => {
+            e.remove();
+        });
+    }
+
+    window.setTimeout(waitforAds, 1);
+})();
+
