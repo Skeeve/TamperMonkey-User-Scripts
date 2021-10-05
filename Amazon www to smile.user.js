378b1e0089edb4023ac3f382572155b228dce323
Stephan Hradek <github@hradek.net>
2021-10-05 15:53:09 +0200

    Add redirect to smile

---
+++ Amazon www to smile.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Amazon www to smile
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.amazon.com/*
+// @match        https://www.amazon.de/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.location.href = document.location.href.replace(/^(https:\/\/)www\./, "$1smile.");
+})();
