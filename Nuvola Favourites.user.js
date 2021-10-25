e6d4db6401b73d073361bbcdef9aaf7387aec0f6
Stephan Hradek <stephan.hradek@eco.de>
2021-10-25 21:11:16 +0200

    add lots of comments

--- Nuvola Favourites.user.js
+++ Nuvola Favourites.user.js
@@ -2,7 +2,7 @@
 // @name         Nuvola Favourites
 // @namespace    http://tampermonkey.net/
 // @version      0.4
-// @description  try to take over the world!
+// @description  try to make nuvola website a bit more comfortable
 // @author       https://github.com/Skeeve
 // @match        https://smartradio.frontier-nuvola.net/portal/content/radios/*
 // @match        https://smartradio.frontier-nuvola.net/portal/content/feeds/*
@@ -11,31 +11,43 @@
 // @grant        GM_addStyle
 // ==/UserScript==
 
+/* eslint-env jquery */
+
 (function doit() {
     'use strict';
 
-    const mainpage = document.location.pathname.match('^/portal/favorites/([0-9a-fA-F]+)(?:/.*)$');
+    const NUVOLA_RELOGIN = 'NuvolaRelogin';
+
+    // Use window.name to transport some information
     let myDevice = window.name;
-    if (myDevice == 'NuvolaRelogin') {
+    // If the name is 'NuvolaRelogin', the window should be closed
+    if (myDevice == NUVOLA_RELOGIN) {
         window.close();
         return;
     }
+
+    // Get the device ID from the URL
+    const mainpage = document.location.pathname.match('^/portal/favorites/([0-9a-fA-F]+)(?:/.*)$');
     if (mainpage !== null) {
         myDevice = mainpage[1];
+        // store the device in window.name
         window.name = myDevice;
         console.log("Device is now", window.name);
         return;
     }
+    // Fail if no device id found
     if (myDevice == "" ) {
         console.log("No device set.");
         return;
     }
 
+    // Enhance all the "Heart-Buttons"
     const buttons = $('a.button.is-small.is-rounded.is-primary');
-    console.log("fixing the buttons:", buttons);
+    console.log("Enhancing the buttons:", buttons);
     let btncount = 0;
     buttons.each(function(idx,elt) {
         ++btncount;
+        // Set a click handler for the button
         $(elt).on("click",function(evt) {
             const lnk = this;
             let data = '';
@@ -48,48 +60,76 @@
             const bgcol= row.css('background-color');
             lnk.blur();
             row.css('background-color', bg);
+            // POST the request
             jQuery.post(lnk.href, data)
             .done(function(data) {
+                // If we were logged out, there should be no logout link in the result page
                 if (data.match(/href="\/portal\/logout"/)) {
+                    // But there is one, so we were still logged in
                     $(lnk).toggleClass('is-outlined');
                     $(lnk).children('i').removeClass('fa-exclamation-triangle').addClass('fa-heart');
                     $(lnk).attr('title', 'Success!');
                 } else {
+                    // Otherwise we were logged out
                     $(lnk).children('i').removeClass('fa-heart').addClass('fa-exclamation-triangle');
                     $(lnk).attr('title', 'Failed!\nMake sure you are logged in.');
+                    // Open a window to log in again.
                     relogin();
                 }
             })
-            .always(function()  {
+            // reset all color changes
+            .always(function() {
                 row.css('background-color', bgcol);
-            })
-            ;
+            });
             return false;
         });
     });
-    console.log("Buttons fixed:", btncount);
+    console.log("Buttons enhanced:", btncount);
+
+    // Now enhance the folder view
     $('tr.directory i.fa-folder').each(function(idx, dir) {
-        console.log(">>>> Found folder", dir);
+        // get the folder's id
         const dir_id = dir.closest('tr').getAttribute('data-sorting').replaceAll(/^\["([^"]+)".*$/g, "$1");
-        console.log(">>>> folder id", dir_id);
+        // Add a class to the table element indicating that the folder is closed.
         $(dir.closest('table')).addClass('closed-' + dir_id);
-        // $(dir).closest('tr').nextAll('tr[data-sorting*=' + dir_id +']').addClass('folder-' + idx);
+        // Add a click handler for opening or closing the folder
         $(dir).on('click', function(evt) {
             const dir = this;
+            // toggle closed <-> open
             $(dir).toggleClass('fa-folder fa-folder-open');
             $(dir).closest('table').toggleClass('open-' + dir_id + ' closed-' + dir_id);
         });
+        // Create a new style for all folder members, hiding them in closed status
         const newStyle = 'table.closed-' + dir_id + ' tr:not(.directory)[data-sorting*="' + dir_id+ '"] { display:none !important; }';
-        console.log(newStyle);
         GM_addStyle(newStyle);
     });
+
+    // Add a style for signalling that an operation failed
     GM_addStyle('i.fa-exclamation-triangle {color:red;}');
 
+    // Relogin function
     function relogin() {
+        // Calculate the position of the login window
+        // It should be centered
+        const w = 500;
+        const h = 770;
+        const top = (screen.height - h) / 2;
+        const left = (screen.width - w) / 2;
+        // Open the window in such a way that it can close itself
         window.open(
             'https://smartradio.frontier-nuvola.net/portal/oauth',
-            'NuvolaRelogin',
-            'fullscreen=no,height=750,location=no,menubar=no,resizable=yes,scrollbars=no,status=no,titlebar=no,toolbar=no,width=500'
+            NUVOLA_RELOGIN,
+            ['fullscreen=no',
+             'height=' + h, 'width=' + w,
+             'top=' + top, 'left=' + left,
+             'location=no',
+             'menubar=no',
+             'resizable=yes',
+             'scrollbars=no',
+             'status=no',
+             'titlebar=no',
+             'toolbar=no',
+            ].join()
         ).opener = null;
     }
 })();
