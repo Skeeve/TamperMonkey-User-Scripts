52d62cf994ec612b591be04e32d8b7784c2d335a
Stephan Hradek <stephan.hradek@eco.de>
2021-12-05 15:58:12 +0100

    English version included

--- Mathekalender Übersicht.user.js
+++ Mathekalender Übersicht.user.js
@@ -5,6 +5,7 @@
 // @description  try to take over the world!
 // @author       https://github.com/Skeeve
 // @match        https://www.mathekalender.de/wp/de/kalender/aufgaben/
+// @match        https://www.mathekalender.de/wp/calendar/challenges/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=mathekalender.de
 // @grant        none
 // ==/UserScript==
@@ -12,13 +13,25 @@
 (function() {
     'use strict';
 
-    let wpb_column = document.querySelector('a[href^="https://www.mathekalender.de/wp/de/kalender/aufgaben/aufgabe-"]');
+    const aufgaben = document.querySelectorAll(`
+        a[href^="https://www.mathekalender.de/wp/de/kalender/aufgaben/aufgabe-"],
+        a[href^="https://www.mathekalender.de/wp/calendar/challenges/challenge-"]
+    `);
+    if (aufgaben == null) return;
+
+    let wpb_column = aufgaben[0];
+    let AntwortFehlt = ' - Antwort fehlt';
+    let Antwort = ' - Antwort:';
+    if (wpb_column.href.match(/challenges/)) {
+        AntwortFehlt = ' - No answer yet';
+        Antwort = ' - Answer:';
+    }
     while ( !wpb_column.classList.contains('wpb_column')) {
         wpb_column = wpb_column.parentNode;
     }
     wpb_column.className = '';
 
-    document.querySelectorAll('a[href^="https://www.mathekalender.de/wp/de/kalender/aufgaben/aufgabe-"]').forEach( (aufgabe, idx) => {
+    aufgaben.forEach( (aufgabe, idx) => {
         fetch(aufgabe.href)
         .then( response => response.text() )
         .then( html => {
@@ -28,10 +41,10 @@
             let solElt = undefined;
             if (selected === null) {
                 solElt = document.createElement('i');
-                solElt.innerText = ' - Antwort fehlt';
+                solElt.innerText = AntwortFehlt;
             } else {
                 solElt = document.createElement('span');
-                solElt.innerText = " - Antwort: " + selected.value + " -" + loesungsText(doc, selected.index);
+                solElt.innerText = Antwort + selected.value + " -" + loesungsText(doc, selected.index);
             }
             aufgabe.parentNode.append(solElt);
         })
@@ -40,7 +53,8 @@
 	function loesungsText(doc, index) {
 		let text = '';
 		doc.querySelectorAll('h4').forEach( h4 => {
-			if (h4.textContent != 'Antwortmöglichkeiten:') return;
+			if (h4.textContent != 'Antwortmöglichkeiten:'
+               && h4.textContent != 'Possible answers:') return;
 			text = h4.parentNode.querySelectorAll('ol li')[index].textContent;
 		});
 		return text;
